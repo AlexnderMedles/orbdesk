b059a729f39d7ed032b7bbc539a86698decf7c50
Alexander <alexander@example.com>
2026-02-11 17:40:28 +0600

    Fix: New next config and total cleanup

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Это заставит Vercel чистить папку при каждой сборке
+  distDir: '.next',
+};
+
+export default nextConfig;
